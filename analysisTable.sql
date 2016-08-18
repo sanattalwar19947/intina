cd40b811a338fbc1b7801e8cc172dfdb59ef2ce2
ayushkalani <ayush_kalani2@yahoo.co.in>
2016-08-17 21:20:45 -0700

    tables

---
+++ analysisTable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE MONTHLYREPORT
+(
+WAREHOUSE_NO varchar2(40) not null,
+AVG_REQ number(5) not null,
+AVG_SERVED number(5) not null,
+MISS number(5) not null,
+EFF number(5) not null
+);
+CREATE TABLE technician_avg_status
+(
+WAREHOUSE_NO varchar2(40) not null,
+OCCUPIED number(5) not null,
+FREE number(5) not null,
+OTW number(5) not null
+);
