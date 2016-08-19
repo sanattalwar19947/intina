54debb86cfc1efa87c029a032a28efe6f8dd2d4f
ayushkalani <ayush_kalani2@yahoo.co.in>
2016-08-18 21:06:31 -0700

    Add files via upload

---
+++ jugaad.sql
@@ -0,0 +1,59 @@
+CREATE TABLE MONTHLYREPORT1
+(
+WAREHOUSE_NO varchar2(40) not null,
+AVG_REQ number(5) not null,
+AVG_SERVED number(5) not null,
+MISS number(5) not null,
+EFF number(5) not null
+);
+CREATE TABLE MONTHLYREPORT2
+(
+WAREHOUSE_NO varchar2(40) not null,
+AVG_REQ number(5) not null,
+AVG_SERVED number(5) not null,
+MISS number(5) not null,
+EFF number(5) not null
+);
+CREATE TABLE MONTHLYREPORT3
+(
+WAREHOUSE_NO varchar2(40) not null,
+AVG_REQ number(5) not null,
+AVG_SERVED number(5) not null,
+MISS number(5) not null,
+EFF number(5) not null
+);
+CREATE TABLE MONTHLYREPORT4
+(
+WAREHOUSE_NO varchar2(40) not null,
+AVG_REQ number(5) not null,
+AVG_SERVED number(5) not null,
+MISS number(5) not null,
+EFF number(5) not null
+);
+Month 1:
+w1	860	757	103	88
+w2	560	470	90	84
+w3	800	688	112	86
+w4	860	670	190	78
+w5	900	855	45	95
+
+Month 2:
+w1	940	837	103	89
+w2	860	740	120	86
+w3	600	528	72	88
+w4	790	664	126	84
+w5	780	749	31	96
+
+Month 3:
+w1	820	738	82	90
+w2	890	774	116	87
+w3	980	872	108	89
+w4	760	646	114	85
+w5	690	669	21	97
+
+Month 4:
+w1	800	736	64	92
+w2	980	882	98	90
+w3	760	714	46	94
+w4	870	775	95	89
+w5	1000	990	10	99
