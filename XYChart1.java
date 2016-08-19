59dc1405b4774a345656a55bab9b76c7b01d0e21
sanattalwar19947 <talwar_jyotika@rediffmail.com>
2016-08-18 17:32:23 -0700

    Update XYChart1.java

--- XYChart1.java
+++ XYChart1.java
@@ -6,9 +6,14 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.data.xy.XYDataset;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,7 +22,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class XYChart1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+	ResultSet rs;
+	PreparedStatement pst;
     public XYChart1() {
         super();
         // TODO Auto-generated constructor stub
@@ -25,13 +31,50 @@ public class XYChart1 extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		XYSeries series = new XYSeries("XYGraph");
+		ToConnect cn=new ToConnect();
+		Connection con=cn.getConnection();
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement
+			                 ("select eff from five_month where month like 'jan'");
+		rs=ps.executeQuery();
+		while(rs.next())
+		{
+		series.add(1,rs.getInt("EFF"));
+		}
+		ps =con.prepareStatement
+                ("select eff from five_month where month like 'feb'");
+		rs=ps.executeQuery();
+		while(rs.next())
+		{
+			series.add(2,rs.getInt("EFF"));
+		}
+		ps =con.prepareStatement
+                ("select eff from five_month where month like 'mar'");
+		rs=ps.executeQuery();
+		while(rs.next())
+		{
+			series.add(3,rs.getInt("EFF"));
+		}
+		ps =con.prepareStatement
+                ("select eff from five_month where month like 'apr'");
+		rs=ps.executeQuery();
+		while(rs.next())
+		{
+			series.add(4,rs.getInt("EFF"));
+		}
+		ps =con.prepareStatement
+                ("select eff from five_month where month like 'may'");
+		rs=ps.executeQuery();
+		while(rs.next())
+		{
+			series.add(5,rs.getInt("EFF"));
+		}
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
-		int as=0;
-		series.add(1, a1);
-		series.add(2, a2);
-		series.add(3, a3);
-		series.add(4, 78);
-		series.add(5, 82);
 		// Add the series to your data set
 		XYSeriesCollection dataset = new XYSeriesCollection();
 		dataset.addSeries(series);
@@ -55,3 +98,4 @@ public class XYChart1 extends HttpServlet {
 	}
 
 }
+
